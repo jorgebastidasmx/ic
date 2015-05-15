88f10d7eae451bd56360a980f37810eb47135e63
Jorge Bastidas <jorgebastidas@multiplica.com>
2015-05-15 17:04:39 -0500

    FIXED SHELL FOR PROD

--- pentaho/shell/load_scores_epm.sh
+++ pentaho/shell/load_scores_epm.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-export PENTAHO_BASE_DIR=/opt/pentaho/pdi-ce-5.2.0.0-209
+export PENTAHO_BASE_DIR=/opt/pentaho/pdi-ce-5.2.0.0-209/
+#export PENTAHO_BASE_DIR=/Applications/pentaho/
 export KETTLE_JNDI_ROOT=$PENTAHO_BASE_DIR/simple-jndi 
 
 dia=`date +%EY%m%d%H%M|awk {'print $1'}`
 DIRBINPTHO=$PENTAHO_BASE_DIR
-DIRJOB_GENERAR_SCORE=/opt/tools/PENTAHO_JOBS
+
+#Ruta donde se encuentran los archivos KTR y KJB
+DIRJOB_GENERAR_SCORE=/opt/EPM/JOBS/
+#DIRJOB_GENERAR_SCORE=/Users/indiegente/Documents/multiplica/ic/pentaho/
+
+#Ruta de la carpeta LOG
 DIRLOG=/tmp/
+
+#Ruta de la carpeta donde se generan los TXT de sujetos para la carga de score
 PATH_FILES=$DIRJOB_GENERAR_SCORE
 
-PARAMS="-param:COD_PAIS=1 -param:FILE=/home/pentaho5/PENTAHO_JOBS/EPM/ -param:SCHEMA_EPM=EPM_MSG -param:KTR-LOAD=CREATE-FILES.ktr -param:KTR-SBS=SBS.ktr -param:KTR-SCORE=SCORE-1.ktr -param:KTR-SICOM=SICOM.ktr -param:KTR-RISK-PREDICTOR=SCORE-RISK-PREDICTOR.ktr -param:PATH=$PATH_FILES" 
-PARAMS2="-param:SCHEMA_EPM=EPM_MSG param:KTR-LOAD-CLIENTES-SEGMENTOS=LOAD-SUJETOS-SEGMENTOS.ktr -param:KTR-LOAD-CARTERA-CLIENTES=LOAD-CARTERA-CLIENTES.ktr -param:PATH_FILES=$PATH_FILES" 
+PARAMS="-param:COD_PAIS=1 -param:FILE=$DIRJOB_GENERAR_SCORE -param:SCHEMA_EPM=EPM_MSG -param:KTR-LOAD=CREATE-FILES.ktr -param:KTR-SBS=SBS.ktr -param:KTR-SCORE=SCORE-1.ktr -param:KTR-SICOM=SICOM.ktr -param:KTR-RISK-PREDICTOR=SCORE-RISK-PREDICTOR.ktr -param:PATH=$PATH_FILES" 
 
-#echo $PARAMS
-#echo "Inicio proceso carga Scores `date`" >> $DIRLOG/Process_LoadScore_$dia.log
-echo "Inicio proceso carga de cartera de clientes `date`" >> $DIRLOG/Process_LoadCarteraCliente$dia.log
+echo $PARAMS
+echo "Inicio proceso carga Scores `date`" >> $DIRLOG/Process_LoadScore_$dia.log
+$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-SCORES-FROM-DATABASE.kjb $PARAMS -Level:Rowlevel >> $DIRLOG/Process_LoadScore_$dia.log
 
-$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-CARTERA-CLIENTES.kjb $PARAMS2 -Level:Rowlevel >> $DIRLOG/Process_LoadCarteraCliente$dia.log
-$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-SUJETOS-SEGMENTOS.kjb $PARAMS2 -Level:Rowlevel >> $DIRLOG/Process_LoadSujetosSegmentos$dia.log
-$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-SCORES-FROM-DATABASE.kjb $PARAMS -Level:Basic >> $DIRLOG/Process_LoadScore_$dia.log
+#PARAMS2="-param:SCHEMA_EPM=EPM_MSG param:KTR-LOAD-CLIENTES-SEGMENTOS=LOAD-SUJETOS-SEGMENTOS.ktr -param:KTR-LOAD-CARTERA-CLIENTES=LOAD-CARTERA-CLIENTES.ktr -param:PATH_FILES=$PATH_FILES" 
+#echo "Inicio proceso carga de cartera de clientes `date`" >> $DIRLOG/Process_LoadCarteraCliente$dia.log
+#$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-CARTERA-CLIENTES.kjb $PARAMS2 -Level:Rowlevel >> $DIRLOG/Process_LoadCarteraCliente.log
+#$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-SUJETOS-SEGMENTOS.kjb $PARAMS2 -Level:Rowlevel >> $DIRLOG/Process_LoadSujetosSegmentos.log
