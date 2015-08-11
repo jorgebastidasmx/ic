5dba5afcbf15b551b281c6cc9e7ade10744a864c
davis_mx <davis@multiplica.com>
2015-08-11 15:46:46 -0500

    Se crea kjb para carga de Billing

---
+++ pentaho/shell/load_billing.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PENTAHO_BASE_DIR=/opt/pentaho/pdi-ce-5.0.1-stable/
+#export PENTAHO_BASE_DIR=/Applications/pentaho/
+export KETTLE_JNDI_ROOT=$PENTAHO_BASE_DIR/simple-jndi 
+
+dia=`date +%EY%m%d%H%M|awk {'print $1'}`
+DIRBINPTHO=$PENTAHO_BASE_DIR
+
+#Ruta donde se encuentran los archivos KTR y KJB
+DIRJOB_GENERAR_BILLING=/opt/tools/Pentaho/EPM/LOAD_SCORE/JOB/
+
+#Ruta de la carpeta LOG
+DIRLOG=/opt/tools/Pentaho/EPM/LOAD_SCORE/JOB/log/
+#Ruta de la carpeta donde se generan los TXT de sujetos para la carga de score
+PATH_FILES=$DIRJOB_GENERAR_BILLING
+
+PARAMS="-param:COD_PAIS=1 -param:SCHEMA_EPM=EPM_MSG -param:PATH=$PATH_FILES -param:KTR-BILLING=LOAD_BILLING.ktr" 
+
+echo $PARAMS
+echo "Inicio proceso facturacion `date`" >> $DIRLOG/Process_Facturacion.log
+$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_BILLING/LOAD-BILLING.kjb $PARAMS -Level:Rowlevel >> $DIRLOG/Process_Facturacion.log
--- pentaho/shell/load_sbs_sicom_diario.sh
+++ pentaho/shell/load_sbs_sicom_diario.sh
@@ -17,6 +17,6 @@ DIRLOG=/opt/tools/Pentaho/EPM/LOAD_SCORE/JOB/log/
 #Ruta de la carpeta donde se generan los TXT de sujetos para la carga de score
 PATH_FILES=$DIRJOB_GENERAR_SCORE
 
-PARAMS="-param:COD_PAIS=1 -param:FILE=$DIRJOB_GENERAR_SCORE -param:KTR-LOAD=CREATE-FILES-DIARIO-SBS-SICOM.ktr -param:SCHEMA_EPM=EPM_MSG -param:KTR-SBS=SBS.ktr -param:KTR-SICOM=SICOM.ktr -param:PATH=$PATH_FILES -param:KTR-CLEAN-CACHE=CLEAN_CACHE.ktr" 
+PARAMS="-param:COD_PAIS=1 -param:FILE=$DIRJOB_GENERAR_SCORE -param:KTR-LOAD=CREATE-FILES-DIARIO-SBS-SICOM.ktr -param:SCHEMA_EPM=EPM_MSG -param:KTR-SBS=SBS.ktr -param:KTR-SICOM=SICOM.ktr -param:KTR-DECOMICRO=DECO-MICRO.ktr -param:PATH=$PATH_FILES -param:KTR-CLEAN-CACHE=CLEAN_CACHE.ktr" 
 echo "Inicio proceso carga SBS SICOM `date`" >> $DIRLOG/Process_LoadSBS$dia.log
 $DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD_SBS_SICOM.kjb $PARAMS -Level:Rowlevel >> $DIRLOG/Process_LoadSBS$dia.log
