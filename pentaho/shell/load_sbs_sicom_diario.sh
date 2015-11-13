b4af379e581c5ad024258cd393710fe990c6b6c9
Davis Rixi <davis@multiplica.com>
2015-11-13 10:44:38 -0500

    Regularizacion de componentes pentaho

--- pentaho/shell/load_sbs_sicom_diario.sh
+++ pentaho/shell/load_sbs_sicom_diario.sh
@@ -4,7 +4,7 @@ export PENTAHO_BASE_DIR=/opt/pentaho/pdi-ce-5.2.0.0-209/
 #export PENTAHO_BASE_DIR=/Applications/pentaho/
 export KETTLE_JNDI_ROOT=$PENTAHO_BASE_DIR/simple-jndi
 
-dia=`date +%EY%m%d%H%M|awk {'print $1'}`
+FECHA=`date +%Y-%m-%d_%H%M%S`
 DIRBINPTHO=$PENTAHO_BASE_DIR
 
 #Ruta donde se encuentran los archivos KTR y KJB
@@ -17,6 +17,6 @@ DIRLOG=/opt/tools/Pentaho52/EPM/LOAD_SCORE/JOB/log/
 #Ruta de la carpeta donde se generan los TXT de sujetos para la carga de score
 PATH_FILES=$DIRJOB_GENERAR_SCORE
 
-PARAMS="-param:COD_PAIS=1 -param:FILE=$DIRJOB_GENERAR_SCORE -param:KTR-LOAD=CREATE-FILES.ktr -param:SCHEMA_EPM=EPM_MSG -param:KTR-SBS=SBS.ktr -param:KTR-SICOM=SICOM.ktr -param:PATH=$PATH_FILES -param:KTR-DECOMICRO=DECO-MICRO.ktr -param:KTR-CLEAN-CACHE=CLEAN_CACHE.ktr" 
-echo "Inicio proceso carga SBS SICOM `date`" >> $DIRLOG/Process_LoadSBS$dia.log
-$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD_SBS_SICOM.kjb $PARAMS -Level:Rowlevel >> $DIRLOG/Process_LoadSBS$dia.log 
+PARAMS="-param:COD_PAIS=1 -param:FILE=$DIRJOB_GENERAR_SCORE -param:KTR-LOAD=CREATE-FILES-DIARIO-SBS-SICOM.ktr -param:SCHEMA_EPM=EPM_MSG -param:KTR-SBS=SBS.ktr -param:KTR-SICOM=SICOM.ktr -param:KTR-DECOMICRO=DECO-MICRO.ktr -param:PATH=$PATH_FILES -param:KTR-CLEAN-CACHE=CLEAN_CACHE.ktr" 
+echo "Inicio proceso carga SBS SICOM `date`" >> $DIRLOG/Process_LoadSBS$FECHA.log
+$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD_SBS_SICOM.kjb $PARAMS -Level:Rowlevel >> $DIRLOG/Process_LoadSBS$FECHA.log
--- pentaho/shell/load_scores_epm.sh
+++ pentaho/shell/load_scores_epm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PENTAHO_BASE_DIR=/opt/pentaho/pdi-ce-5.0.1-stable/
+export PENTAHO_BASE_DIR=/opt/pentaho/pdi-ce-5.2.0.0-209/
 #export PENTAHO_BASE_DIR=/Applications/pentaho/
 export KETTLE_JNDI_ROOT=$PENTAHO_BASE_DIR/simple-jndi 
 
@@ -8,20 +8,20 @@ dia=`date +%EY%m%d%H%M|awk {'print $1'}`
 DIRBINPTHO=$PENTAHO_BASE_DIR
 
 #Ruta donde se encuentran los archivos KTR y KJB
-DIRJOB_GENERAR_SCORE=/opt/tools/Pentaho/EPM/LOAD_SCORE/JOB/
+DIRJOB_GENERAR_SCORE=/opt/tools/Pentaho52/EPM/LOAD_SCORE/JOB/
 #DIRJOB_GENERAR_SCORE=/Users/indiegente/Documents/multiplica/ic/pentaho/
 
 #Ruta de la carpeta LOG
-DIRLOG=/opt/tools/Pentaho/EPM/LOAD_SCORE/JOB/log/
+DIRLOG=/opt/tools/Pentaho52/EPM/LOAD_SCORE/JOB/log/
 #Ruta de la carpeta donde se generan los TXT de sujetos para la carga de score
 PATH_FILES=$DIRJOB_GENERAR_SCORE
 
-PARAMS="-param:COD_PAIS=1 -param:FILE=$DIRJOB_GENERAR_SCORE -param:SCHEMA_EPM=EPM_MSG -param:KTR-LOAD=CREATE-FILES.ktr -param:KTR-SBS=SBS.ktr -param:KTR-SCORE=SCORE-1.ktr -param:KTR-SICOM=SICOM.ktr -param:KTR-RISK-PREDICTOR=SCORE-RISK-PREDICTOR.ktr -param:PATH=$PATH_FILES" 
-PARAMS_TEST="-param:COD_PAIS=1 -param:JURIDICA=juridica.txt -param:NATURAL=natural.txt -param:FILE=$DIRJOB_GENERAR_SCORE -param:SCHEMA_EPM=EPM_MSG -param:KTR-LOAD=CREATE-FILES.ktr -param:KTR-SBS=SBS.ktr -param:KTR-SCORE=SCORE-1.ktr -param:KTR-SICOM=SICOM.ktr -param:KTR-RISK-PREDICTOR=SCORE-RISK-PREDICTOR.ktr -param:PATH=$PATH_FILES" 
+PARAMS="-param:COD_PAIS=1 -param:FILE=$DIRJOB_GENERAR_SCORE -param:SCHEMA_EPM=EPM_MSG -param:KTR-LOAD=CREATE-FILES-DIARIO.ktr -param:KTR-SCORE=SCORE-1.ktr -param:KTR-RISK-PREDICTOR=SCORE-RISK-PREDICTOR.ktr  -param:KTR-MICROFINANZAS_2_0=MICROFINANZAS_2_0.ktr -param:PATH=$PATH_FILES -param:KTR-CLEAN-CACHE=CLEAN_CACHE.ktr" 
+PARAMS_TEST="-param:COD_PAIS=1 -param:JURIDICA=juridica.txt -param:NATURAL=natural.txt -param:FILE=$DIRJOB_GENERAR_SCORE -param:SCHEMA_EPM=EPM_MSG -param:KTR-LOAD=CREATE-FILES-DIARIO.ktr -param:KTR-SCORE=SCORE-1.ktr -param:KTR-RISK-PREDICTOR=SCORE-RISK-PREDICTOR.ktr -param:PATH=$PATH_FILES -param:KTR-CLEAN-CACHE=CLEAN_CACHE.ktr"  
 
 echo $PARAMS
 echo "Inicio proceso carga Scores `date`" >> $DIRLOG/Process_LoadScore.log
-$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-SCORES-FROM-DATABASE.kjb $PARAMS -Level:Rowlevel >> $DIRLOG/Process_LoadScore.log
+$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-SCORES-FROM-DATABASE.kjb $PARAMS -Level:Rowlevel >> $DIRLOG/Process_LoadScore$dia.log
 #$DIRBINPTHO/kitchen.sh -file=$DIRJOB_GENERAR_SCORE/LOAD-SCORES-FROM-FILE-TEST.kjb $PARAMS_TEST -Level:Rowlevel >> $DIRLOG/Process_LoadScore.log
 
 #PARAMS2="-param:SCHEMA_EPM=EPM_MSG param:KTR-LOAD-CLIENTES-SEGMENTOS=LOAD-SUJETOS-SEGMENTOS.ktr -param:KTR-LOAD-CARTERA-CLIENTES=LOAD-CARTERA-CLIENTES.ktr -param:PATH_FILES=$PATH_FILES" 
